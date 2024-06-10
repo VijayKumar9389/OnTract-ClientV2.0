9105709723c960bb40b58c07a0446a263e1a8ba4
VijayKumar9389 <vijaymkumar2095@gmail.com>
2024-06-10 13:25:50 -0600

    Fix user table overflow

--- src/pages/Users/components/UserTable/UserTable.tsx
+++ src/pages/Users/components/UserTable/UserTable.tsx
@@ -1,23 +1,25 @@
 import {User} from "../../../../models/auth.models.ts";
 import UserTableRow from "./UserTableRow.tsx";
 
-const UserTable: React.FC<{users: User[]}> = ({users}) => {
+const UserTable: React.FC<{ users: User[] }> = ({users}) => {
 
     return (
-        <table className="user-table">
-            <thead>
-            <tr>
-                <th>Username</th>
-                <th>Admin Status</th>
-                <th>Actions</th>
-            </tr>
-            </thead>
-            <tbody>
-            {users.map((user: User) => (
-                <UserTableRow user={user} key={user.id}/>
-            ))}
-            </tbody>
-        </table>
+        <div className="table-wrapper">
+            <table className="user-table">
+                <thead>
+                <tr>
+                    <th>Username</th>
+                    <th>Admin Status</th>
+                    <th>Actions</th>
+                </tr>
+                </thead>
+                <tbody>
+                {users.map((user: User) => (
+                    <UserTableRow user={user} key={user.id}/>
+                ))}
+                </tbody>
+            </table>
+        </div>
     );
 }
 
